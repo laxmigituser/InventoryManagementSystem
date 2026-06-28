0913b0e9809d8bffc08e4938e657e2a3e97f4188
Laxmi <laxminarayansamantaray2000@gmail.com>
2026-06-28 12:19:57 +0530

    Lesson 12 - Implement ProductRepository using Spring Data JPA

---
+++ backend/src/main/java/com/lns/inventory/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.lns.inventory.repository;
+
+import com.lns.inventory.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
