bfb1e7b6a522cc837f619feb89094f425d5dab6c
Laxmi <laxminarayansamantaray2000@gmail.com>
2026-06-28 12:09:09 +0530

    Lesson 11 - Create Product entity using JPA and Hibernate

---
+++ backend/src/main/java/com/lns/inventory/entity/Product.java
@@ -0,0 +1,28 @@
+package com.lns.inventory.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private Integer quantity;
+}
